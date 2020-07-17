b2ec2d77ce572c121c18a220b094037d3a629dd8
ltamang977 <ltamang977@gmail.com>
2020-07-17 10:31:06 -0700

    Initial Commit

---
+++ src/test/java/org/lv/accessdatamongodb/AccessDataMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lv.accessdatamongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessDataMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
